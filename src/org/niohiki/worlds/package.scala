fdf51713c76fbec39876a31f7301b8c902723ceb
niohiki <santicristo@gmail.com>
2015-02-05 23:15:09 +0100

    Fixed some problems with quasiquotes and inside

--- src/org/niohiki/worlds/package.scala
+++ src/org/niohiki/worlds/package.scala
@@ -3,30 +3,10 @@ package org.niohiki
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox.Context
 import scala.reflect.macros.TypecheckException
+import scala.reflect.macros.TypecheckException
+import scala.reflect.macros.TypecheckException
 
 package object worlds {
-  def lulz(x: Any): Unit = macro lulz_impl
-  def lulz_impl(c: Context)(x: c.Tree): c.Tree = {
-    import c.universe._
-
-    val ident = Ident(TermName(c.freshName("dfsf")))
-
-    q"""{
-          val $ident = $x
-          println($ident)
-        }"""
-
-    x match {
-      case q"$iden.$met($thin)" => {
-        val nam: String = thin.toString
-        val num = 5
-        val two = 1 + 1
-        val four = q"$num+$two"
-        q"""println($nam)"""
-      }
-    }
-  }
-
   def inside(inputWorld: World)(f: => Unit): Unit = macro inside_impl
   def inside_impl(c: Context)(inputWorld: c.Tree)(f: c.Tree): c.Tree = {
     import c.universe._
@@ -35,38 +15,47 @@ package object worlds {
         override def transform(tree: Tree): Tree = {
           f(super.transform(tree))
         }
-      }.transform(t))
+      }.transform(c.typecheck(t)))
     }
 
-    val tokenSymbol = (q"localWorld").symbol
-    val returnTree = treeTransform(f, _ match {
-      case q"$tag->[$types]($property)" if c.typecheck(
-        tag).tpe <:< c.typeOf[Tag] =>
-        q"$inputWorld.get[$types]($tag,$property)"
+    val worldIdentifier = Ident(TermName(c.freshName("internalWorld")))
+    val tokenSymbol = c.typecheck(q"localWorld").symbol
+    val getValueTree = treeTransform(f, _ match {
       case q"$tag-<[$types]($property)" if c.typecheck(
         tag).tpe <:< c.typeOf[Tag] =>
-        q"($inputWorld.get[$types]($tag,$property))()"
-      case q"$property:=($value)" => q"$inputWorld.set($property,$value)"
-      case other => {
-        val symbol = c.typecheck(other, silent = true).symbol
+        q"$tag.->[$types]($property).apply()"
+      case default => default
+    })
+    val getTree = treeTransform(getValueTree, _ match {
+      case q"$tag->[$types]($property)" if c.typecheck(
+        tag).tpe <:< c.typeOf[Tag] =>
+        q"localWorld.get[$types]($tag,$property)"
+      case q"$property:=($value)" if c.typecheck(
+        property).tpe <:< c.typeOf[PropertyBin[_]] =>
+        q"localWorld.set($property,$value)"
+      case default => default
+    })
+    val tokenTree = treeTransform(getTree, _ match {
+      case item => {
+        val symbol = c.typecheck(item, silent = true).symbol
         if (symbol != null && symbol.fullName.equals(tokenSymbol.fullName)) {
-          inputWorld
-        } else other
+          worldIdentifier
+        } else item
       }
     })
-    returnTree
+    q"val $worldIdentifier=$inputWorld;$tokenTree"
   }
 
   def inspect(e: Any): Unit = macro inspect_impl
-  def inspect_impl(c: Context)(e: c.Expr[_]): c.Expr[Unit] = {
+  def inspect_impl(c: Context)(e: c.Tree): c.Tree = {
     import c.universe._
     print_impl(c)(showRaw(e))
   }
 
-  def print_impl(c: Context)(a: Any): c.Expr[Unit] = {
+  def print_impl(c: Context)(a: Any): c.Tree = {
     import c.universe._
-    val e = c.Expr(Literal(Constant(a)))
-    reify { println(e.splice) }
+    val e = a.toString
+    q"println($e)"
   }
 
   implicit def property2value[T](property: PropertyBin[T]): T = property()
